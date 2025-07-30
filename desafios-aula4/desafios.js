6e3ff0ccd0fab9a5eb2d18b77be68a8db1134a90
Daniel Gonçalves <83424429+dev-danielg@users.noreply.github.com>
2025-07-30 17:57:54 -0300

    Add files via upload

---
+++ desafios-aula4/desafios.js
@@ -0,0 +1,7 @@
+const listaGenerica = []
+const linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python']
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang')
+const nomesDePessoas = ['Daniel', 'João', 'Pedro']
+console.log(`Primeiro nome: ${nomesDePessoas[0]}
+Segundo nome: ${nomesDePessoas[1]}
+Terceiro nome: ${nomesDePessoas[2]}`)
